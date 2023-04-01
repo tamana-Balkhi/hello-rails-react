ea52ec4b4b6a849bbfcad5df684dc62575976227
tamana-Balkhi <tamana.balkhi1377@gmail.com>
2023-04-01 16:18:34 +0430

    create route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'root/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+    root 'root#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :messages, only: [:index, :show], format: :json
+    end
+  end
 end
